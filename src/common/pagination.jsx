2e1783915eca1f59666182253032c5efe52f8032
yasmeen1999 <yasmeenaleemkhan786@gmail.com>
2020-06-17 01:47:15 +0530

    added list

--- src/common/pagination.jsx
+++ src/common/pagination.jsx
@@ -1,14 +1,36 @@
 import React from "react";
+import PropTypes from "prop-types";
+import _ from "lodash";
+
 const Pagination = (props) => {
+  const { iteamCount, pageSize, currentPage, onPageChange } = props;
+  console.log(currentPage);
+
+  const pagesCount = Math.ceil(iteamCount / pageSize);
+  if (pagesCount === 1) return null;
+  const pages = _.range(1, pagesCount + 1);
+
   return (
     <nav>
       <ul className='pagination'>
-        <li className='page-item'>
-          <a className='page-link'>1</a>
-        </li>
+        {pages.map((page) => (
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}>
+            <a className='page-link' onClick={() => onPageChange(page)}>
+              {page}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
+};
+Pagination.propTypes = {
+  iteamCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
 };
 
 export default Pagination;
